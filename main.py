0b08523e4ae71b197e6866d75e6da211ec3aee04
nick <jnicholastorres1@gmail.com>
2023-04-13 15:05:17 -0500

    commit

--- main.py
+++ main.py
@@ -13,30 +13,39 @@ import sklearn.decomposition
 from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import scale
-
+import seaborn as sns
 
 def create_graph():
 
+    #load data of an 8x8 image
     ds = load_digits()
-    dig_data = scale(ds.data)
-    num = len(np.unique(ds.target))
-    red = PCA(n_components=2).fit_transform(dig_data)
 
+    #standardize data along an axis
+    dig_data = scale(ds.data)
+
+    #filer data into unique values
+    num = len(np.unique(ds.target))
+    
+    #pcauce dimensionality to 2 dimensions
+    pca = PCA(n_components=2).fit_transform(dig_data)
+
+    #Set boundaries for graph
     h = 0.02
-    xmin, xmax = red[:, 0].min() - 1, red[:,0].max() + 1
-    ymin, ymax = red[:, 1].min() - 1, red[:, 1].max() + 1
+    xmin, xmax = pca[:, 0].min() - 1, pca[:,0].max() + 1
+    ymin, ymax = pca[:, 1].min() - 1, pca[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(xmin, xmax, h), np.arange(ymin, ymax, h))
 
+    # Models for each distance
     models = [
         (
             KMedoids(metric="manhattan", n_clusters=3,
                      init="heuristic", max_iter=2), "Manhattan metric",
         ),
         (
-            KMedoids(metric="euclidean", n_clusters=3,
+            KMedoids(metric="euclidean", n_clusters=4,
                      init="heuristic", max_iter=2), "Euclidean metric",
         ),
-        (KMedoids(metric="cosine", n_clusters=3, init="heuristic",
+        (KMedoids(metric="cosine", n_clusters=5, init="heuristic",
                   max_iter=2), "Cosine metric",), ]
 
     # number of rows = integer(ceiling(number of model variants/2))
@@ -51,8 +60,8 @@ def create_graph():
 
     for i, (model, description) in enumerate(models):
         # Fit each point in the mesh to the model
-        model.fit(red)
-        # Predict the labels for points in the mesh
+        model.fit(pca)
+        # Ppcaict the labels for points in the mesh
         Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
         # Put the result  into a color plot
         Z = Z.reshape(xx.shape)
@@ -69,7 +78,7 @@ def create_graph():
             origin="lower",  # set origin as lower left corner of the axes
         )
         plt.plot(
-            red[:, 0], red[:, 1], "k.", markersize=2, alpha=0.3
+            pca[:, 0], pca[:, 1], "k.", markersize=2, alpha=0.3
         )
         # Plot the centroids as white cross marks
         centroids = model.cluster_centers_
@@ -79,7 +88,7 @@ def create_graph():
             marker="x",
             s=169,  # marker’s size (points^2)
             linewidths=3,  # width of boundary lines
-            color="w",  # white color for centroids markings
+            color="r",  # white color for centroids markings
             zorder=10,  # drawing order of axes
         )
         # describing text of the tuple will be title of the subplot
@@ -94,7 +103,10 @@ def create_graph():
         "K-Medoids algorithm implemented with different metrics\n\n",
         fontsize=20,  # size of the fonts
     )
+
+    input("Press To Continue")
     plt.show()
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
